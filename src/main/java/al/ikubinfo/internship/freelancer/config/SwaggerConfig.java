9fe8c3cd2666a1a62dcee97dc7785db92e08d3ba
Aleksander Ndoci <ndocialex@gmail.com>
2021-03-17 21:12:56 +0100

    fix SwaggerConfig

--- src/main/java/al/ikubinfo/internship/freelancer/config/SwaggerConfig.java
+++ src/main/java/al/ikubinfo/internship/freelancer/config/SwaggerConfig.java
@@ -1,19 +1,28 @@
 package al.ikubinfo.internship.freelancer.config;
 
+import al.ikubinfo.internship.freelancer.entity.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static springfox.documentation.builders.PathSelectors.any;
 import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String AUTH_METHOD = "OAuth(JWT Access Token)";
+    private static final String API_KEY_NAME = "Authorization";
+    private static final String API_KEY_PASS_AS = "header";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -21,7 +30,30 @@ public class SwaggerConfig {
                 .select()
                 .apis(withClassAnnotation(RestController.class))
                 .paths(any())
+                .build()
+                .securitySchemes(this.securityScheme())
+                .securityContexts(this.securityContext());
+    }
+
+    private List<SecurityContext> securityContext() {
+        SecurityContext securityContext = SecurityContext.builder()
+                .securityReferences(Collections.singletonList(
+                        SecurityReference.builder()
+                                .reference(AUTH_METHOD)
+                                .scopes(scopes())
+                                .build()))
                 .build();
+        return Collections.singletonList(securityContext);
+    }
+
+    private List<SecurityScheme> securityScheme() {
+        return Collections.singletonList(new ApiKey(AUTH_METHOD, API_KEY_NAME, API_KEY_PASS_AS));
+    }
+
+    private AuthorizationScope[] scopes() {
+        return Stream.of(Role.values())
+                .map(a -> new AuthorizationScope(a.name(), a.name()))
+                .toArray(AuthorizationScope[]::new);
     }
 
     private ApiInfo apiInfo() {
